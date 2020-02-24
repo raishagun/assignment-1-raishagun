ee74b11a637fbe197a429330438cdd72b3e9e74f
raishagun <shagun.gla_cs18@gla.ac.in>
2020-02-24 20:39:21 +0530

    create node class and its getter and setters

--- src/definition/ContactList.java
+++ src/definition/ContactList.java
@@ -24,4 +24,30 @@ public class ContactList implements ContactListADT<Person> {
     public void view() {
 
     }
+    private static   class Node {
+
+        private Person data;
+        private Node next;
+
+        public Node(Person data,Node next) {
+            this.data = data;
+            this. next=null;
+        }
+
+        public Person getData() {
+            return data;
+        }
+
+        public void setData(Person data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
 }
