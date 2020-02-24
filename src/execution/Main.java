6d9cc6cff982bc2a81f501ffc2a45f1683a64b77
raishagun <shagun.gla_cs18@gla.ac.in>
2020-02-24 21:01:08 +0530

    create object of ContactList

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -1,9 +1,14 @@
 package execution;
 
+import definition.ContactList;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner ob = new Scanner(System.in);
+        ContactList conts = new ContactList();
+
+
     }
 }
