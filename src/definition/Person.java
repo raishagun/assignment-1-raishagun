5567085c6b50c52d07b4bb910d787366255dcb4d
raishagun <shagun.gla_cs18@gla.ac.in>
2020-02-24 20:31:27 +0530

    getter and setter methods for firstname

--- src/definition/Person.java
+++ src/definition/Person.java
@@ -7,4 +7,14 @@ public class Person {
        private String lastName;
         private ArrayList<Long> contacts;
         private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
 }
