e206ee4fc8dbadf7a237a974dca7030192f6c9f6
raishagun <shagun.gla_cs18@gla.ac.in>
2020-02-24 21:03:37 +0530

    create menu driven list

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -11,6 +11,16 @@ public class Main {
         ContactList conts = new ContactList();
         MaintainContactList sc = new MaintainContactList();
 
+        while (true) {
+            System.out.println("Welcome to Shagun's Contact List App\n" +
+                    "Press 1 to add a new contact\n" +
+                    "Press 2 to view all contacts\n" +
+                    "Press 3 to search for a contact\n" +
+                    "Press 4 to delete a contact\n" +
+                    "Press 5 to exit program");
 
+            int choice = ob.nextInt();
+
+        }
     }
 }
