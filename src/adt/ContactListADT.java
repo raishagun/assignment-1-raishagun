ac4c2f940a0e4b5557d6bcfbb1ee586f8bc1e260
raishagun <shagun.gla_cs18@gla.ac.in>
2020-02-24 20:24:28 +0530

    create abstract method add remove search view to contact in adt

--- src/adt/ContactListADT.java
+++ src/adt/ContactListADT.java
@@ -1,5 +1,10 @@
 package adt;
 
-public interface ContactListADT {
+public interface ContactListADT<Person> {
+    void add(Person data);
+    void remove(int item);
+    void search(String name);
+    void view();
+
 
 }
---
+++ src/definition/ContactList.java
@@ -0,0 +1,26 @@
+package definition;
+
+import adt.ContactListADT;
+
+public class ContactList implements ContactListADT<Person> {
+
+    @Override
+    public void add(Person data) {
+
+    }
+
+    @Override
+    public void remove(int item) {
+
+    }
+
+    @Override
+    public void search(String name) {
+
+    }
+
+    @Override
+    public void view() {
+
+    }
+}
